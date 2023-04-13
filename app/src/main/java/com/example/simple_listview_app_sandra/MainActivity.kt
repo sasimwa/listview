d9ae1748e54be765fd6d6de784e911d9bb77ea63
eMobilis <sandrasasimwa@gmail.com>
2023-04-13 10:00:04 +0300

    listview

---
+++ app/src/main/java/com/example/simple_listview_app_sandra/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.simple_listview_app_sandra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
